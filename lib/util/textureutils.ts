3b14847ded45319418a59c8c596caf07d057bdd2
Elijah Bevers <elijah.bevers@oit.edu>
2025-08-03 01:57:35 -0700

    Added test texture for local dev

--- lib/util/textureutils.ts
+++ lib/util/textureutils.ts
@@ -191,5 +191,6 @@ export const defaultTextures: Texture[] = [
     { img: '/textures/noise1.png' },
     { img: '/textures/noise2.png' },
     { img: '/textures/noise3.png' },
-    { img: '/textures/noise4.png' }
+    { img: '/textures/noise4.png' },
+    { img: '/textures/moon_cubemap.png' }
 ];
